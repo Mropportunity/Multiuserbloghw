036d0aca967c4f2cb48f57c56b0a508dc347820f
Cue Morris <delquan.morris@gmail.com>
2017-02-06 13:01:24 -0600

    1 of 3

--- main.py
+++ main.py
@@ -35,6 +35,20 @@ class FizzBuzzHandler(Handler):
         n = n and int(n)
         self.render('fizzbuzz.html', n=n)
 
+
+class Rot13(Handler):
+
+    def get(self):
+        self.render('rot13-form.html')
+
+    def post(self):
+        rot13 = ''
+        text = self.request.get('text')
+        if text:
+            rot13 = text.encode('rot13')
+
+        self.render('rot13-form.html', text=rot13)
+
 app = webapp2.WSGIApplication([('/', MainPage),
                                ('/fizzbuzz', FizzBuzzHandler),
                                ('/fizzbuzz', FizzBuzzHandler),
